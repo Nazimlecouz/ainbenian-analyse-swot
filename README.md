6bd751b8766e0c56bfe2f4eb9f3488e32759096d
Nazimlecouz <nazim.fifa88@gmail.com>
2025-11-01 22:05:50 +0100

    Updates

---
+++ README.md
@@ -0,0 +1,5 @@
+AIN BENIAN - ANALYSE SWOT (fixed)
+
+Commands:
+  npm install
+  npm start
---
+++ src/components/PresentationMode.jsx
@@ -0,0 +1,2 @@
+import React from 'react';
+export default function PresentationMode(){ return <div className='p-8 text-center'>Mode Présentation</div>; }
